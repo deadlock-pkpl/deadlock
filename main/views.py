b5cc24bc7ee03562bf83a86716ad077f061c009d
raihanheriandry <raihanmaulana.heriandry@gmail.com>
2026-03-29 09:08:40 +0700

    ubah views.py

--- main/views.py
+++ main/views.py
@@ -1,10 +1,4 @@
 from django.shortcuts import render
 
 def show_main(request):
-    context = {
-        'npm' : '240123456',
-        'name': 'Haru Urara',
-        'class': 'PBP A'
-    }
-
-    return render(request, "main.html", context)
+    return render(request, "main.html")
