a92dd3e5442b13b0a246882f2d7fce992c36096c
AnggaTriSetiawan <anggatrisetiawan81@gmail.com>
2026-03-31 19:42:38 +0700

    Fitur Login OAuth

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from main.views import show_main
+from . import views
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', show_main, name='show_main'),
+    path('', views.show_main, name='show_main'),
+    path('auth/login/', views.start_google_login, name='google_login'),
+    path('auth/callback/', views.google_callback, name='google_callback'),
+    path('auth/logout/', views.logout_view, name='logout'),
+    path('auth/colour/', views.change_colour, name='change_colour'),
+    path('auth/font/', views.change_font, name='change_font'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import login as django_login, logout as django_logout
+from django.contrib.auth.models import User
+from .services import GoogleOAuthService
+from django.conf import settings
+from django.http import HttpResponseForbidden
+
+oauth_service = GoogleOAuthService()
 
 def show_main(request):
-    return render(request, "main.html")
+    is_member = False
+    if request.user.is_authenticated:
+        is_member = request.user.email in settings.GROUP_MEMBERS
+    return render(request, "main.html", {
+        "is_member": is_member
+    })
+
+# Redirect user langsung ke Google
+def start_google_login(request):
+    return redirect(oauth_service.get_authorization_url())
+
+def google_callback(request):
+    code = request.GET.get('code')
+    
+    if not code:
+        return redirect('main:show_main')  
+
+    tokens = oauth_service.get_tokens(code)
+    if not tokens or 'access_token' not in tokens:
+        return redirect('main:show_main')   
+    
+    user_data = oauth_service.get_user_info(tokens['access_token'])
+    if not user_data or 'email' not in user_data:
+        return redirect('main:show_main')  
+    
+    user, created = User.objects.get_or_create(
+        email=user_data['email'],
+        defaults={'username': user_data['email'].split('@')[0]}
+    )
+    
+    django_login(request, user)
+    return redirect('main:show_main')  
+
+def logout_view(request):
+    if request.method == "POST":
+        django_logout(request)
+    return redirect('main:show_main')
+
+def change_colour(request):
+    if not request.user.is_authenticated:
+        return HttpResponseForbidden("Harus login dulu.")
+    
+    if request.user.email not in settings.GROUP_MEMBERS:
+        return HttpResponseForbidden("Kamu bukan anggota kelompok.")
+    return render(request, "colour.html", {
+        "is_member": True
+    })
+
+def change_font(request):
+    if not request.user.is_authenticated:
+        return HttpResponseForbidden("Harus login dulu.")
+    
+    if request.user.email not in settings.GROUP_MEMBERS:
+        return HttpResponseForbidden("Kamu bukan anggota kelompok.")
+    return render(request, "font.html", {
+        "is_member": True
+    })
